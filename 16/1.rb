f5a3f3868b0ba8e07eab828f51d0859026d3856d
Ferenc Gerlits <fgerlits@gmail.com>
2022-12-16 14:31:24 +0100

    day 16 part one

--- 16/1.rb
+++ 16/1.rb
@@ -4,10 +4,22 @@ require 'set'
 require_relative 'common'
 
 cave = Cave.new(ARGF)
-vertices = cave.nodes_with_flow
 graph = graph(cave)
 
+vertices = cave.nodes_with_flow
+vertices = vertices - ['AT', 'TO', 'NA', 'XX', 'UD', 'YD']
+
 totals = vertices.permutation.map do |path|
+  path = path.map do |step|
+    if step == 'FI'
+      ['FI', 'AT', 'TO']
+    elsif step == 'KB'
+      ['KB', 'NA', 'XX', 'UD', 'YD']
+    else
+      step
+    end
+  end.flatten(1)
+
   time = 0
   current = 'AA'
   total = 0
@@ -20,6 +32,7 @@ totals = vertices.permutation.map do |path|
     current = step
     total += (30 - switched_on_at) * cave.flow_at(step)
   end
+
   [path, total]
 end
 
