373bf59f158141a78be7ff3419a055f338b56adc
Ferenc Gerlits <fgerlits@gmail.com>
2022-12-19 21:43:35 +0100

    this may work, but it's too slow

--- 19/1.rb
+++ 19/1.rb
@@ -2,5 +2,15 @@
 
 require_relative 'common'
 
-input = parse_input(ARGF)
-p input
+blueprints = parse_input(ARGF).to_h
+
+resources = {'ore' => 0, 'clay' => 0, 'obsidian' => 0, 'geode' => 0}
+robots = {'ore' => 1, 'clay' => 0, 'obsidian' => 0, 'geode' => 0}
+nodes = [Node.new(0, resources, robots)]
+
+rules = blueprints[1]
+24.times do
+  nodes = nodes.map{|node| node.next_nodes(rules)}.flatten(1).sort.uniq
+  puts nodes
+  puts
+end
--- 19/common.rb
+++ 19/common.rb
@@ -6,6 +6,8 @@ def parse_input(stream)
 end
 
 class Rule
+  attr_reader :product
+
   def initialize(sentence)
     sentence =~ /Each (\S+) robot costs (\d+) ore( and (\d+) ([^ .]+).?)?/
     @product = $1
@@ -26,22 +28,71 @@ class Rule
   end
 end
 
-def all_ways_to_apply(rules, resources)
-  # TODO
+def all_ways_to_apply_using_first(rules, resources)
+  rule = rules.first
+  if rule.can_apply?(resources)
+    others = all_ways_to_apply(rules, rule.apply(resources))
+    if others.empty?
+      [[rule]]
+    else
+      others.map{|rule_seq| [rule] + rule_seq}
+    end
+  else
+    []
+  end
 end
 
-class Node
-  def initialize(time, resources, robots, pending_robots)
-    @time, @resources, @robots, @pending_robots = time, resources, robots, pending_robots
+def all_ways_to_apply(rules, resources)
+  (0...rules.size).map do |i|
+    all_ways_to_apply_using_first(rules[i..], resources)
+  end.flatten(1) + [[]]
+end
+
+def produce_resources(resources, robots)
+  new_resources = resources.clone
+  robots.each do |robot_type, number|
+    new_resources[robot_type] += number
   end
+  new_resources
+end
+
+def build_robots(resources, robots, rule_seq)
+  new_resources, new_robots = resources.clone, robots.clone
+  rule_seq.each do |rule|
+    new_resources = rule.apply(new_resources)
+    new_robots[rule.product] += 1
+  end
+  [new_resources, new_robots]
+end
+    
+class Node
+  include Comparable
+
+  attr_reader :time, :resources, :robots
+
+  def initialize(time, resources, robots)
+    @time, @resources, @robots = time, resources, robots
+  end
+
+  def to_s = "time = #{@time}, resources = #{@resources}, robots = #{@robots}"
 
   def next_nodes(rules)
-    # TODO
-  end
+    time = @time + 1
+    increased_resources = produce_resources(@resources, @robots)
 
-  def create_resources
-    @robots.each do |robot_type, number|
-      @resources[robot_type] += number
+    all_ways_to_apply(rules, @resources).map do |rule_seq|
+      resources, robots = build_robots(increased_resources, @robots, rule_seq)
+      Node.new(time, resources, robots)
     end
   end
+
+  def <=>(other)
+    [@time, @resources.to_a, @robots.to_a] <=> [other.time, other.resources.to_a, other.robots.to_a]
+  end
+
+  def eql?(other)
+    [@time, @resources, @robots] == [other.time, other.resources, other.robots]
+  end
+
+  def hash = [@time, @resources, @robots].hash
 end
