58b32c59227a18ac0441d5ccd6117679c8201840
Ferenc Gerlits <fgerlits@gmail.com>
2022-12-24 10:23:41 +0100

    day 24 part one -- works, but may be too slow

--- 24/1.rb
+++ 24/1.rb
@@ -1,6 +1,36 @@
 #!/usr/bin/env ruby
 
+require 'set'
 require_relative 'common'
 
+def length_of_shortest_path(grid)
+  visited = Set.new([grid])
+  todo_list = [[grid, 0]]
+  steps = 0
+
+  while !todo_list.empty?
+    grid, num_steps = todo_list.shift
+
+    if grid.at_exit?
+      return num_steps
+    end
+
+    if num_steps > steps
+      puts num_steps
+      steps = num_steps
+    end
+
+    grid = grid.step_blizzards
+
+    neighbors = grid.neighbors
+    neighbors.each do |neighbor|
+      if !visited.include?(neighbor)
+        visited << neighbor
+        todo_list << [neighbor, num_steps + 1]
+      end
+    end
+  end
+end
+    
 grid = parse_input(ARGF)
-puts grid
+puts length_of_shortest_path(grid)
--- 24/common.rb
+++ 24/common.rb
@@ -4,7 +4,12 @@ def parse_input(stream)
   Grid.new(stream.to_a)
 end
 
+DIRECTIONS = ['<', '>', 'v', '^']
+
 class Grid
+  attr_reader :blizzards, :my_pos
+  attr_writer :blizzards, :my_pos
+
   def initialize(lines)
     @x_size = lines.size - 2
     @y_size = lines[0].chomp.size - 2
@@ -12,20 +17,81 @@ class Grid
     blizzards = []
     lines.each_with_index do |row, x|
       row.each_char.with_index do |c, y|
-        if ['<', '>', 'v', '^'].include?(c)
+        if DIRECTIONS.include?(c)
           blizzards << [x, y, c]
         end
       end
     end
     @blizzards = Set.new(blizzards)
 
-    @my_pos = lines[0].index('.')
+    @entrance = lines[0].index('.')
     @exit = lines[-1].rindex('.')
+
+    @my_pos = [0, @entrance]
+  end
+
+  def hash = [@my_pos, @blizzards].hash
+
+  def eql?(other)
+    @my_pos == other.my_pos && @blizzards.eql?(other.blizzards)
+  end
+
+  def neighbors
+    ns = [[0, 0], [-1, 0], [1, 0], [0, -1], [0, 1]].map do |direction|
+      if can_move?(direction)
+        move(direction)
+      end
+    end.compact
+  end
+
+  def can_move?(direction)
+    x, y = @my_pos.zip(direction).map{|coord, diff| coord + diff}
+    if x == 0
+      y == @entrance
+    elsif x >= 1 && x <= @x_size && y >= 1 && y <= @y_size
+      DIRECTIONS.none?{|dir| @blizzards.include?([x, y, dir])}
+    elsif x == @x_size + 1
+      y == @exit
+    end
+  end
+
+  def move(direction)
+    new_grid = self.clone
+    new_grid.my_pos = @my_pos.zip(direction).map{|coord, diff| coord + diff}
+    new_grid
+  end
+
+  def step_blizzards
+    new_grid = self.clone
+    new_grid.blizzards = @blizzards.map{|blizzard| step(blizzard)}
+    new_grid
+  end
+
+  def step(blizzard)
+    x, y, c = blizzard
+    case c
+    when '<'
+      if y == 1 then y = @y_size + 1 end
+      [x, y - 1, c]
+    when '>'
+      if y == @y_size then y = 0 end
+      [x, y + 1, c]
+    when 'v'
+      if x == @x_size then x = 0 end
+      [x + 1, y, c]
+    when '^'
+      if x == 0 then x = @x_size + 1 end
+      [x - 1, y, c]
+    end
+  end
+
+  def at_exit?
+    @my_pos == [@x_size + 1, @exit]
   end
 
   def to_s
     top = '#' * (@y_size + 2)
-    top[@my_pos] = '.'
+    top[@entrance] = '.'
 
     bottom = '#' * (@y_size + 2)
     bottom[@exit] = '.'
@@ -34,12 +100,20 @@ class Grid
       row = '#' + '.' * @y_size + '#'
       @blizzards.each do |bx, by, bc|
         if bx == x
-          row[by] = bc
+          if row[by] == '.'
+            row[by] = bc
+          elsif DIRECTIONS.include?(row[by])
+            row[by] = '2'
+          elsif row[by] =~ /\d/
+            row[by] = (row[by].to_i + 1).to_s
+          end
         end
       end
       row
     end
-
-    ([top] + middle + [bottom]).join("\n")
+    
+    grid = [top] + middle + [bottom]
+    grid[@my_pos[0]][@my_pos[1]] = 'E'
+    "#{@my_pos}\n" + grid.join("\n")
   end
 end
