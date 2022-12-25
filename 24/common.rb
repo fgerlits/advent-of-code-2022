32317730e2ea38bc42f9bb7623b8cd11f6c27b25
Ferenc Gerlits <fgerlits@gmail.com>
2022-12-25 23:23:04 +0100

    refactoring

--- 24/common.rb
+++ 24/common.rb
@@ -135,3 +135,42 @@ class State
     "#{@my_pos}\n" + grid.join("\n")
   end
 end
+
+def length_of_shortest_path(state)
+  visited = Set.new([state])
+  todo_list = [[state, 0]]
+  steps = 0
+
+  while !todo_list.empty?
+    state, num_steps = todo_list.shift
+
+    if state.at_exit?
+      return num_steps
+    end
+
+    if num_steps > steps
+      steps = num_steps
+    end
+
+    neighbors = state.neighbors
+    neighbors.each do |neighbor|
+      if !visited.include?(neighbor)
+        visited << neighbor
+        todo_list << [neighbor, num_steps + 1]
+      end
+    end
+  end
+end
+
+def find_all_blizzards(blizzards)
+  seen_at = {blizzards => 0}
+  (1..).each do |n|
+    blizzards = blizzards.step
+    seen = seen_at[blizzards]
+    if !seen.nil?
+      return seen_at.map{|blizzards, n| [n, blizzards.blizzards]}.to_h
+    else
+      seen_at[blizzards] = n
+    end
+  end
+end
